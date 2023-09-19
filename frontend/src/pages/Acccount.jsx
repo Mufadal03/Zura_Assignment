e51787841465b1dae719eaedc382399d7017cba4
Mufadal03 <sadrimufadal@gmail.com>
2023-09-19 14:08:36 +0530

    Initializing screens

---
+++ frontend/src/pages/Acccount.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Acccount = () => {
+  return (
+    <div>Acccount</div>
+  )
+}
+
+export default Acccount
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'purple': '#7E22CE',
+      'white': '#FFFFFF',
+      'light-purple': '#F3E8FF',
+      'activeBlack': '#211935',
+      'gray': '#1D1B201F',
+      'light-gray':'#1D1B2014'
+    },
     extend: {},
   },
   plugins: [],
