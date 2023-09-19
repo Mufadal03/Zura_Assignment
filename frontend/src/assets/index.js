7f9b93bed25d3fad91a8c7454d9d4fa8ff6133d7
Mufadal03 <sadrimufadal@gmail.com>
2023-09-19 14:06:21 +0530

    assets Icons for Navigational Drawer

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,24 @@
+import Logo from './Icons/Logo.svg'
+import Setting from './Icons/Setting.svg'
+import ActiveSetting from './Icons/activeSettings.svg'
+const Menus = [
+    {
+        name: 'Projects',
+        link: '/project/upload',
+    },
+    {
+        name: 'Widget Configurations',
+        link: '/project/configuration',
+    },
+    {
+        name: 'Deployment',
+        link: '/',
+    },
+    {
+        name: 'Pricing',
+        link: '/',
+    },
+    
+]
+
+export {Logo,Menus,Setting,ActiveSetting}
