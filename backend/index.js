7e545046dc16c774a0c9398776452bbc215b60d1
Mufadal03 <sadrimufadal@gmail.com>
2023-09-22 04:46:12 +0530

    PORT Error fixed

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express()
 app.use(cors({
-    origin:['http://localhost:4000']
+    origin:['http://localhost:3000']
 }))
 app.use(express.json())
 
